740b92fe672660b309b4443f58579924e4e43f71
Alvin Li <aflip24@gmail.com>
2023-03-17 04:21:44 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+const square = (number1) => {
+    return number1 * number1;
+}
+
+const add = (number1, number2) => {
+    return number1 + number2;
+}
